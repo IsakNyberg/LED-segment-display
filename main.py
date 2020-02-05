0f30b7e3d5582a216312ef9f92fe4875a552ecb8
Isak Nyberg <36712644+IsakNyberg@users.noreply.github.com>
2020-02-05 23:27:59 +0100

    Made a framework for individual led selection in the new device class
    - Device class has a list of all the registers
    - each entry in the register is 8 bit
    - send sends to the chip
    - toggle_led sets led to on/off

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 
-import time
+from led_index import *
 
 def to_bytearray(address, data):
     return bytes([address]) + bytes([address]) + bytes([data])
-
-
-serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial, cascaded=1)
 
 device.data(to_bytearray(0b0010, 0b00101010))
 
@@ -18,13 +14,28 @@ for i in range(0):
     device.data(to_bytearray(0b0010, i))
     time.sleep(0.1)
 
-def send(address, data):
-    pass
+class Device:
+    def __init__(self):
+        self.serial = spi(port=0, device=0, gpio=noop())
+        self.device = max7219(self.serial, cascaded=1)
 
+        self.registries = [0x00 for i in range(8)]
 
+    def send(address, data):
+        if address > 15 or data > 256:
+            raise ValueError("data must be < 256 and address < 15")
+        serial_data = bytes(address) + bytes(data)
+        device.data(serial_data)
 
-def toggle_led(led_number, on):
-    pass
+    def toggle_led(led_number, on):
+        registry_index = led_registry_index[led_number]
+        bit_index = led_bit_index[led_number]
+        if on:
+            self.registries[registry_index] |= bit_index
+        else:
+            # ~ in python is not nice :(
+            self.registries[registry_index] &= 0xFF - bit_index 
 
+        self.send(registry_index, self.registries[registry_index])
 
 
