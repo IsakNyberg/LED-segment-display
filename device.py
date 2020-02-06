6ca30e005fd9cf7c708a4d0313ffa7977fee6130
Isak Nyberg <36712644+IsakNyberg@users.noreply.github.com>
2020-02-06 23:09:41 +0100

    Added comments

--- device.py
+++ device.py
@@ -6,6 +6,7 @@ from luma.core.interface.serial import spi, noop
 from led_index import *
 from segment import to_segment
 
+
 class Device:
     def __init__(self):
         self.serial = spi(port=0, device=0, gpio=noop())
@@ -17,8 +18,11 @@ class Device:
         return str(self.registries)
 
     def send(self, address, data):
+        """
+        Sets the registry data at a given address
+        """
         if address > 15 or data > 256:
-            raise ValueError("Data must be < 256 and address < 15")
+            raise ValueError('Data must be < 256 and address < 15')
         serial_data = bytes([address, data])
         self.device.data(serial_data)
         if address < 9 and address > 1:
@@ -26,12 +30,12 @@ class Device:
 
     def toggle_led_register(self, led_number, on):
         """
-        Only edits the enternal register and returns which register was edited
-        This is done so that if an entire register of leds was change a new SPI
-        transfer is not neccesary for every individual led.
+        Only edits the internal register and returns which register was edited
+        This is done so that if an entire register of LEDs was change a new SPI
+        transfer is not necessary for every individual led.
         """
-        if led_number < 2 or led_number > 21:
-            raise ValueError("{0} is not a valid led number".format(led_number))
+        if 2 > led_number or led_number > 21:
+            raise ValueError('{0} is not a valid LED number'.format(led_number))
         registry_index = led_registry_index[led_number]
         bit_index = led_bit_index[led_number]
         if on:
@@ -47,45 +51,48 @@ class Device:
         else:
             on = 0x00
         
-        regestry_indexes = [0x05, 0x06]      
-        for index in set(regestry_indexes):
+        registry_indexes = [0x05, 0x06]
+        for index in set(registry_indexes):
             self.send(index, on)
             self.registries[index-1] = on
 
     def toggle_led(self, led_number, on):
-        regestry_indexes = []
+        registry_indexes = []
         if type(led_number) is list:
             for led in led_number:
-                regestry_indexes.append(self.toggle_led_register(led, on))
+                registry_indexes.append(self.toggle_led_register(led, on))
         else:
-            regestry_indexes.append(self.toggle_led_register(led_number, on))
+            registry_indexes.append(self.toggle_led_register(led_number, on))
 
-        for index in set(regestry_indexes):
+        for index in set(registry_indexes):
             self.send(index, self.registries[index])
     
     def value_led(self, num_led):
         self.bar_led(False)
         if num_led > 16 or num_led < 0:
-            raise ValueError("num_led must be between 0 than 16")
+            raise ValueError('num_led must be between 0 than 16')
         
-        self.toggle_led([9,8,7,6,5,4,3,2,17,16,15,14,13,12,11,10][:num_led], True)
+        self.toggle_led([9, 8, 7, 6, 5, 4, 3, 2, 17, 16, 15, 14, 13, 12, 11, 10][:num_led], True)
     
     def middle_led(self, num_led):
         self.bar_led(False)
         if num_led > 7 or num_led < -9:
-            raise ValueError("num_led must be between -9 than 7")
+            raise ValueError('num_led must be between -9 than 7')
         
         if num_led > 0:
-            self.toggle_led([3,4,5,6,7,8,9][:num_led],True)
+            self.toggle_led([3, 4, 5, 6, 7, 8, 9][:num_led], True)
         elif num_led < 0:
-            self.toggle_led([2,17,16,15,14,13,12,11,10][:-num_led], True)
+            self.toggle_led([2, 17, 16, 15, 14, 13, 12, 11, 10][:-num_led], True)
             
-    def green(self, on=1):
+    def green(self, on=True):
         self.toggle_led(21, on)
+
     def yellow(self, on=1):
         self.toggle_led(20, on)
+
     def red(self, on=1):
         self.toggle_led(19, on)
+
     def blue(self, on=1):
         self.toggle_led(18, on)
 
@@ -97,8 +104,8 @@ class Device:
     
     def brightness(self, level):
         if level > 15 or level < 0:
-            raise ValueError("Brightness must be between 0 and 15")
-        self.send(0x0A, level)
+            raise ValueError('Brightness must be between 0 and 15')
+        self.send(0x0A, level)  # 0x0A is the code for brightness
     
         
 
--- main.py
+++ main.py
@@ -5,41 +5,48 @@ from time import sleep
 
 if __name__ == "__main__":
     board = device.Device()
-    
-    for i in range(2,17):
+
+    # The value function turn on all LEDs from 0 to the value
+    for i in range(2, 17):
         board.value_led(i)
         sleep(0.1)
-    
+
+    # bar_led set the value of all LEDs to the given value
     board.bar_led(False)
-    
+
+    # segment display of a string with supported character
     board.segment_display("HE O")
-    
     sleep(1)
+
+    # registry specific communication in this cause it puts the two vertical lines between the E and O
     board.send(0x03, 0b00110110)
-    
     sleep(1)
-    board.toggle_led(7, True)
+
+    # toggles the given led to a given value
+    board.toggle_led(7, True)  # single led on
     sleep(0.5)
-    board.toggle_led([4,5,6,7,8,9], True)
+    board.toggle_led([4, 5, 6, 7, 8, 9], True)  # multiple LEDs on
     sleep(0.5)
-    board.toggle_led([5,6,7,8], False)
+    board.toggle_led([5, 6, 7, 8], False)  # multiple LEDs off
     sleep(0.5)
-    board.toggle_led(4, False)
+    board.toggle_led(4, False)  # single led off
     sleep(0.5)
-    board.toggle_led(9, False)
-    
+    board.toggle_led(9, False)  # single led off
     sleep(1)
-    board.red(1)
+
+    # toggles the control LEDs
+    board.red(True)
     sleep(0.5)
     board.red(False)
-    board.green(1)
+    board.green(True)
     sleep(0.5)
     board.yellow(True)
-    board.blue(1)
+    board.blue(True)
     sleep(0.5)
     board.red(True)
-    
     sleep(1)
+
+    # same as value but starts in the middle and can be either negative or positive
     for i in range(8):
         board.middle_led(i)
         sleep(0.5)
@@ -47,16 +54,18 @@ if __name__ == "__main__":
     for i in range(10):
         sleep(0.5)
         board.middle_led(-i)
-    
+
+    # string and float display on the segment display
     board.segment_display("PI=")
     sleep(1)
     board.segment_display(3.142)
-    
     sleep(1)
+
+    # Brightness adjust
     board.bar_led(True)
     for i in range(16):
         board.brightness(i)
-        board.segment_display(i)
+        board.segment_display(i)  # int displayed on segment display
         sleep(1)
     
     sleep(1)
