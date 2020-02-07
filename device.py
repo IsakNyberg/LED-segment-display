f97b2be9417711a25e17300b9ebeb0a6f0a66b47
Isak Nyberg <36712644+IsakNyberg@users.noreply.github.com>
2020-02-07 16:01:29 +0100

    Commented file and improved the if conditionds

--- device.py
+++ device.py
@@ -12,7 +12,8 @@ class Device:
         self.serial = spi(port=0, device=0, gpio=noop())
         self.device = max7219(self.serial, cascaded=1)
         
-        self.registries = [0x00 for i in range(8)]  # this is a copy of the registries on the chip
+        self.registries = [0x00 for i in range(8)]
+        # this is a copy of the registries on the chip registry 1 starts at index 0
         
     def __repr__(self):
         return str(self.registries)
@@ -20,63 +21,88 @@ class Device:
     def send(self, address, data):
         """
         Sets the registry data at a given address
+        :arg address: The address that should be edited
+        :arg data: What to put into the given address.
         """
-        if address > 15 or data > 256:
-            raise ValueError('Data must be < 256 and address < 15')
+        if not(-1 < address < 16):
+            raise ValueError('address must be < 16')
+
+        if not(-1 < data < 256):
+            raise ValueError('Data must be < 256')
+
         serial_data = bytes([address, data])
         self.device.data(serial_data)
-        if address < 9 and address > 1:
+        if 1 < address < 9:  # The address was within the saved register
             self.registries[address-1] = data
 
     def toggle_led_register(self, led_number, on):
         """
+        Private function
         Only edits the internal register and returns which register was edited
         This is done so that if an entire register of LEDs was change a new SPI
         transfer is not necessary for every individual led.
         """
-        if 2 > led_number or led_number > 21:
+        if not (1 < led_number < 22):
             raise ValueError('{0} is not a valid LED number'.format(led_number))
         registry_index = led_registry_index[led_number]
         bit_index = led_bit_index[led_number]
         if on:
             self.registries[registry_index] |= bit_index
         else:
-            # ~ in python is not nice :(
+            # ~ in python is not nice :(     Improvised inversion
             self.registries[registry_index] &= 0xFF - bit_index
         return registry_index
     
     def bar_led(self, on):
+        """
+        Sets all the LEDs in the LED bar to the given value.
+        :arg on: What the status of the LEDs should be set to
+        """
         if on:
             on = 0xff
         else:
             on = 0x00
         
-        registry_indexes = [0x05, 0x06]
+        registry_indexes = [0x05, 0x06]  # The registry for the 16 LEDs in the bar
         for index in set(registry_indexes):
             self.send(index, on)
             self.registries[index-1] = on
 
     def toggle_led(self, led_number, on):
-        registry_indexes = []
+        """
+        Toggles the given LED(s) to a given value
+        :arg led_number: list or int of the affected LED(s) [2:21]
+        :arg on: The state to set the given LED(s) to [True:False].
+        """
+        registry_indexes = []  # to keep track which registries that are changed
         if type(led_number) is list:
             for led in led_number:
                 registry_indexes.append(self.toggle_led_register(led, on))
         else:
             registry_indexes.append(self.toggle_led_register(led_number, on))
 
-        for index in set(registry_indexes):
+        for index in set(registry_indexes):  # update the board after setting all the registries
             self.send(index, self.registries[index])
     
     def value_led(self, num_led):
-        self.bar_led(False)
-        if num_led > 16 or num_led < 0:
+        """
+        Toggles a given number of LEDs in a row
+        :arg num_led: number of LEDs to light [0:16]
+        """
+        self.bar_led(False)  # turn bar off
+        if not (-1 < num_led < 17):
             raise ValueError('num_led must be between 0 than 16')
         
         self.toggle_led([9, 8, 7, 6, 5, 4, 3, 2, 17, 16, 15, 14, 13, 12, 11, 10][:num_led], True)
     
     def middle_led(self, num_led):
-        self.bar_led(False)
-        if num_led > 7 or num_led < -9:
+        """
+        Lights up LEDs from the middle in either direction depending on whether the input is positive
+        or negative
+        :arg num_led: Number of LEDs to light up [-9:7]
+        """
+        self.bar_led(False)  # turn bar off
+        if not (-10 < num_led < 8):
             raise ValueError('num_led must be between -9 than 7')
         
         if num_led > 0:
@@ -85,27 +111,50 @@ class Device:
             self.toggle_led([2, 17, 16, 15, 14, 13, 12, 11, 10][:-num_led], True)
             
     def green(self, on=True):
+        """
+        Toggles the green control light.
+        :arg on: what state to toggle the light to [True:False]
+        """
         self.toggle_led(21, on)
 
     def yellow(self, on=1):
+        """
+        Toggles the yellow control light.
+        :arg on: what state to toggle the light to [True:False]
+        """
         self.toggle_led(20, on)
 
     def red(self, on=1):
+        """
+        Toggles the red control light.
+        :arg on: what state to toggle the light to [True:False]
+        """
         self.toggle_led(19, on)
 
     def blue(self, on=1):
+        """
+        Toggles the blue control light.
+        :arg on: what state to toggle the light to [True:False]
+        """
         self.toggle_led(18, on)
 
     def segment_display(self, display):
+        """
+        Displays the input on the 7 segment display. The length can max be 4 excluding the decimal point
+        for floats.
+        :arg display: The item to be displayed int/str/float
+        """
         segments = to_segment(display)
-        for index in range(4):
-            self.registries[index] = segments[index]
-            self.send(index+1, segments[index])
+        for index in range(0x04):  # segment display is stored in registers 1-4
+            self.registries[index] = segments[index]  # update register
+            self.send(index+1, segments[index])  # send data
     
     def brightness(self, level):
-        if level > 15 or level < 0:
+        """
+        Sets the brightness for the LEDs and the 7 segment display.
+        :arg level: The level the brightness should be set to [0:15]
+        """
+        if not (-1 < level < 16):
             raise ValueError('Brightness must be between 0 and 15')
-        self.send(0x0A, level)  # 0x0A is the code for brightness
-    
-        
 
+        self.send(0x0A, level)  # 0x0A is the code for brightness
